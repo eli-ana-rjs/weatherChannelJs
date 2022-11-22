ca0513105f349488468bad475847137640dc7dc1
elianaMac <elianabellarojas@gmail.com>
2022-11-22 17:09:54 -0300

    Limpiar el input. Mensaje alerta ciudad no existente

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const inputCiudad = document.getElementById('input-ciudad');
 const button = document.querySelector('button');
 
- /* La variable ciudad guarda el valor que el usuario escribe en el input */
+/* La variable ciudad guarda el valor que el usuario escribe en el input */
 let ciudad = inputCiudad.value;
 
 /* Variables del contenedor principal */
@@ -19,21 +19,30 @@ const cargarCiudad = () => {
     document.querySelector(".container").style.visibility = "visible"
 
     /*Pedido AJAX que carga la informaci[on de la ciudad solicitada por el usuario */
-    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + "San%20Francisco"+ "&appid=0ce94f33157d53abdd67e5b603996e78&units=metric" ,
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + "San%20Francisco" + "&appid=0ce94f33157d53abdd67e5b603996e78&units=metric",
 
         function (data) {
-        console.log(data)
+            console.log(data)
 
-        city.textContent = data.name;
-        temperatura.textContent = data.main.temp ;
-        grados.innerHTML = "<sup>°C</sup>";
-        let icon = data.weather[0].icon;
-        img.setAttribute("src", `http://openweathermap.org/img/w/${icon}.png`);
-        descripcion.textContent = data.weather[0].description;
-           
+            /* Cargar informacion al contenedor principal */
+            city.textContent = data.name;
+            temperatura.textContent = data.main.temp;
+            grados.innerHTML = "<sup>°C</sup>";
+            let icon = data.weather[0].icon;
+            img.setAttribute("src", `http://openweathermap.org/img/w/${icon}.png`);
+            descripcion.textContent = data.weather[0].description;
 
-
+        }).fail(() => {
+            /* Mensaje que se muestra en caso de que el usuario ingrese una ciudad que no existe */
+            alert('Ciudad no encontrada');
         })
+
+        // Limpiar el inputCiudad despues de hacer el pedido AJAX
+        ciudad = '';
 }
 
-cargarCiudad()
+
+
+/* Al hacer click en el btn enviar la funcion cargarCiudad realiza el pedido AJAX que trae la informacion de la ciudad pasada en el input */
+button.addEventListener('click', cargarCiudad());
+
